a09a5413a151b082aa7aa5c0f52ae207beeb542c
icarus8050 <icarus8050@naver.com>
2022-03-13 20:42:04 +0900

    add dependency spring boot starter test

--- batch/build.gradle.kts
+++ batch/build.gradle.kts
@@ -6,5 +6,6 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-batch")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.batch:spring-batch-test")
 }
